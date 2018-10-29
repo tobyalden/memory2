89384bc60b8c8a06e2d1656c3c036aafc873388e
Toby Alden <toby.alden@gmail.com>
2018-10-29 12:02:20 -0700

    Finish turret

--- source/entities/Grenade.hx
+++ source/entities/Grenade.hx
@@ -5,6 +5,7 @@ import haxepunk.graphics.*;
 import haxepunk.math.*;
 import haxepunk.Tween;
 import haxepunk.tweens.misc.*;
+import haxepunk.utils.*;
 
 class Grenade extends MemoryEntity {
     public static inline var ACCEL = 0.05;
@@ -14,8 +15,11 @@ class Grenade extends MemoryEntity {
 
     private var sprite:Spritemap;
     private var velocity:Vector2;
-    private var bounceCount:Int;
     private var explodeTimer:Alarm;
+    private var bounceSfxs:Array<Sfx>;
+    private var explodeSfx:Sfx;
+    private var warnSfx:Sfx;
+    private var colorTween:ColorTween;
 
     public function new(x:Float, y:Float, velocity:Vector2) {
         super(x, y);
@@ -27,34 +31,48 @@ class Grenade extends MemoryEntity {
         sprite.add("idle", [0, 1], 5);
         sprite.play("idle");
         setGraphic(sprite);
-        bounceCount = 0;
         explodeTimer = new Alarm(1, TweenType.OneShot);
         explodeTimer.onComplete.bind(function() {
             scene.remove(this);
             scene.add(new Explosion(centerX - 72, centerY - 72));
+            explodeSfx.play();
         });
         addTween(explodeTimer, true);
+        bounceSfxs = new Array<Sfx>();
+        bounceSfxs.push(new Sfx("audio/grenadebounce1.wav"));
+        bounceSfxs.push(new Sfx("audio/grenadebounce2.wav"));
+        bounceSfxs.push(new Sfx("audio/grenadebounce3.wav"));
+        explodeSfx = new Sfx("audio/grenadeexplode.wav");
+        warnSfx = new Sfx("audio/grenadewarn.wav");
+        warnSfx.play();
+        colorTween = new ColorTween(TweenType.OneShot);
+        colorTween.tween(1, 0xFF0000, 0xffff89, 1, 1, Ease.sineIn);
+        addTween(colorTween, true);
     }
 
     override public function update() {
+        var player = scene.getInstance("player");
         var gravity = GRAVITY * Main.getDelta();
         velocity.y += gravity;
         velocity.y = Math.min(velocity.y, MAX_FALL_VELOCITY);
         moveBy(
             velocity.x * Main.getDelta(), velocity.y * Main.getDelta(), "walls"
         );
+        sprite.color = colorTween.color;
         super.update();
     }
 
     public override function moveCollideX(e:Entity) {
-        bounceCount++;
         velocity.x = -velocity.x * BOUNCE_FACTOR;
+        var player = scene.getInstance("player");
+        var volume = (Math.max(1, distanceFrom(player, true)) / 350);
+        bounceSfxs[Std.random(bounceSfxs.length)].play();
         return true;
     }
 
     public override function moveCollideY(e:Entity) {
-        bounceCount++;
         velocity.y = -velocity.y * BOUNCE_FACTOR;
+        bounceSfxs[Std.random(bounceSfxs.length)].play();
         return true;
     }
 }
--- source/entities/Turret.hx
+++ source/entities/Turret.hx
@@ -8,13 +8,13 @@ import haxepunk.tweens.misc.*;
 
 class Turret extends MemoryEntity {
     public static inline var TIME_BETWEEN_LOBS = 3;
-    public static inline var LOB_ACTIVATION_DISTANCE = 300;
     public static inline var LOB_POWER = 5;
 
     private var sprite:Spritemap;
     private var eyes:Spritemap;
     private var lightning:Spritemap;
     private var lobTimer:Alarm;
+    private var lobSfx:Sfx;
 
     public function new(x:Float, y:Float) {
         super(x, y);
@@ -41,12 +41,18 @@ class Turret extends MemoryEntity {
         lobTimer.onComplete.bind(function() {
             lob();
         });
-        addTween(lobTimer, true);
+        addTween(lobTimer);
+        var lobTimerDelay = new Alarm(Math.random(), TweenType.OneShot);
+        lobTimerDelay.onComplete.bind(function() {
+            lobTimer.start();
+        });
+        addTween(lobTimerDelay, true);
+        lobSfx = new Sfx("audio/turretshoot.wav");
     }
 
     private function lob() {
         var player = scene.getInstance("player");
-        if(distanceFrom(player, true) > LOB_ACTIVATION_DISTANCE) {
+        if(!isOnScreen()) {
             return;
         }
         var towardsPlayer = new Vector2(0, 0);
@@ -57,6 +63,7 @@ class Turret extends MemoryEntity {
         towardsPlayer.y = -LOB_POWER;
         var grenade = new Grenade(centerX, top, towardsPlayer);
         scene.add(grenade);
+        lobSfx.play();
     }
 
     override public function update() {
@@ -67,10 +74,7 @@ class Turret extends MemoryEntity {
     private function animation() {
         lightning.visible = stopFlasher.active;
         var player = scene.getInstance("player");
-        if(distanceFrom(player, true) > LOB_ACTIVATION_DISTANCE) {
-            sprite.play("idle");
-        }
-        else if(x > player.x) {
+        if(x > player.x) {
             sprite.play("left");
         }
         else {
